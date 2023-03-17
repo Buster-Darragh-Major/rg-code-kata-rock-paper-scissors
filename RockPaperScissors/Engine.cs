64f87e9ea73e7353af31406c288ce21fd8b47b76
Chris Lambrou <chris.lambrou@red-gate.com>
2023-03-17 15:17:23 +0000

    New projects, Engine, and first test cases

---
+++ RockPaperScissors/Engine.cs
@@ -0,0 +1,36 @@
+﻿namespace RockPaperScissors;
+
+public enum PlayChoice
+{
+    Rock,
+    Paper,
+    Scissors
+}
+
+public enum PlayResult
+{
+    LeftWins,
+    RightWins,
+    Draw
+}
+
+public static class Engine
+{
+    public static PlayResult Play(PlayChoice leftChoice, PlayChoice rightChoice)
+    {
+        return leftChoice switch
+        {
+            PlayChoice.Rock => rightChoice switch
+            {
+                PlayChoice.Scissors => PlayResult.LeftWins,
+                _ => throw new NotSupportedException()
+            },
+            PlayChoice.Scissors => rightChoice switch
+            {
+                PlayChoice.Rock => PlayResult.RightWins,
+                _ => throw new NotSupportedException()
+            },
+            _ => throw new NotSupportedException()
+        };
+    }
+}
---
+++ RockPaperScissorsTests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RockPaperScissors;
+
+namespace RockPaperScissorsTests;
+
+[TestFixture]
+public class EngineTests
+{
+    [Test]
+    public void RockBeatsScissors_LeftWins()
+    {
+        Engine.Play(PlayChoice.Rock, PlayChoice.Scissors).Should().Be(PlayResult.LeftWins);
+    }
+
+    [Test]
+    public void RockBeatsScissors_RightWins()
+    {
+        Engine.Play(PlayChoice.Scissors, PlayChoice.Rock).Should().Be(PlayResult.RightWins);
+    }
+}
